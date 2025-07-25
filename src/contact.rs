79f083f32a17017c9fe316af5695497d3cc9d63b
ParsaFaghani <dark.cs.coder@gmail.com>
2025-07-26 01:44:19 +0330

    create base phone book

---
+++ src/contact.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+use colored::*;
+use prettytable::{Table, Row, Cell};
+use dialoguer::Input;
+
+#[derive(Debug)]
+pub struct Contact {
+    pub phone_numbers: Vec<String>,
+
+}
+
+
+pub fn add_contact(contacts: &mut HashMap<String, Contact>, name: &str, cntct: Contact) {
+    contacts.insert(name.to_string(), cntct);
+}
+
+pub fn print_contacts(contacts: &HashMap<String, Contact>) {
+    let mut table = Table::new();
+    table.add_row(Row::new(vec![
+        Cell::new("Name"),
+        Cell::new("Number(s)"),
+    ]));
+
+    for (key, value) in contacts.iter() {
+        let numbers_str = value.phone_numbers.join(", ");
+        table.add_row(Row::new(vec![
+            Cell::new(key),
+            Cell::new(&numbers_str),
+                                                                    ]));
+    }
+
+    table.printstd();
+
+    // println!("{}", "Raw Debug View:".bright_black());
+    // println!("{:#?}", contacts);
+}
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::collections::HashMap;
+use colored::*;
+use prettytable::{Table, Row, Cell};
+use dialoguer::Input;
+use std::process::Command;
+
+fn clear_screen() {
+        Command::new("clear").status().unwrap();
+}
+
+mod contact;
+
+
+fn main() {
+    let mut contacts: HashMap<String, contact::Contact> = HashMap::new();
+
+    loop {
+        println!("{}","Choose an option".red());
+        println!("{}","1 .Print all exist contacts".blue());
+        println!("{}","2 .Add contact".blue());
+        println!("{}","3 .Exit.".blue());
+
+        let Choice: String = Input::new()
+            .with_prompt("Enter Choice: ")
+            .interact_text()
+            .unwrap();
+        clear_screen();
+        match Choice.trim() {
+            "1" => {
+                contact::print_contacts(&contacts);
+            },
+            "2" => {
+                let name: String = Input::new()
+                    .with_prompt("Name")
+                    .interact_text()
+                    .unwrap();
+
+                let phone_num: String = Input::new()
+                    .with_prompt("Phone Number")
+                    .interact_text()
+                    .unwrap();                    
+                let mut cntct = contact::Contact {                             phone_numbers: Vec::new(),
+                };
+                cntct.phone_numbers.push(phone_num.to_string());
+                contact::add_contact(&mut contacts, &name, cntct);
+            },
+            "3" => break,
+            _ => {println!("{}","Invalid Choice".red()); },
+        }
+
+        // println!("Hello, world!");
+    }
+}
